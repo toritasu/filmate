d76c1c4f5ff069c8712ae4bf9eab8ce44a52c1e6
TORITASU <tt.tuttofare@gmail.com>
2021-06-15 01:11:40 +0900

    TypeScript環境構築 / HelloWorld

---
+++ pages/work.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    {{ text }}
+  </div>
+</template>
+
+<script lang="ts">
+import Logo from '~/components/Logo.vue'
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      text: '' as string
+    }
+  },
+  components: {
+    Logo
+  },
+  created() {
+    this.text = 'Hello World!'
+  }
+})
+</script>
+
+<style scoped style="scss">
+
+</style>
