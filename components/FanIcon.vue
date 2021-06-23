cf8f69bf839ced1f1aace045b5e1a1f1e218b2c8
TORITASU <tt.tuttofare@gmail.com>
2021-06-23 15:29:39 +0900

    コンポーネント：ユーザーアイコン

---
+++ components/FanIcon.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  a.fan(
+    :href="`user/${fan.id}`"
+  )
+    img(:src="fan.icon" :alt="fan.name")
+</template>
+
+<script>
+export default {
+  name: 'FanIcon',
+  props: {
+    fan: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.fan {
+  display: inline-block;
+  border-radius: 100%;
+  border: 1px solid #909090;
+  img {
+    width: 35px;
+    height: 35px;
+    border-radius: 100%;
+    object-fit: cover;
+  }
+}
+</style>
